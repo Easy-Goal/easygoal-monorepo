6b0abd776d4e937932fb48faf09c47bb048e96d9
malconn <malconaugusto@outlook.com>
2026-03-02 19:02:35 -0300

    feat: adicionando packages atualizados conforme ajuste em regra do sso

--- packages/auth/src/hooks/useSSOLogin.ts
+++ packages/auth/src/hooks/useSSOLogin.ts
@@ -38,6 +38,11 @@ export function useSSOLogin(config: SSOLoginConfig) {
     localStorage.clear();
     sessionStorage.clear();
 
+    document.cookie.split(";").forEach((cookie) => {
+      const name = cookie.split("=")[0].trim();
+      document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    });
+
     window.location.href = `${ssoUrl}/auth/signout`;
   };
 
